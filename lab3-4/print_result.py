0cba3dabf8ca7201230728138913087bc8e80470
exp008 <154838747+exp008@users.noreply.github.com>
2023-12-27 02:24:43 +0300

    Create print_result.py

---
+++ lab3-4/print_result.py
@@ -0,0 +1,12 @@
+def print_result(func):
+    def wrapper(*args, **kwargs):
+        a = func(*args, **kwargs)
+        if isinstance(a, list):
+            for i in args: print(args)
+        elif isinstance(a, dict):
+            for i, j in a.items():
+                print(i, " = ", j)
+        else:
+            print(a)
+        return a
+    return wrapper
