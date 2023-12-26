bd5bb86e1b43a2f43b84eb0508720dd67e7aa49e
exp008 <154838747+exp008@users.noreply.github.com>
2023-12-27 02:50:24 +0300

    Update circlef.py

--- lab2/circlef.py
+++ lab2/circlef.py
@@ -3,16 +3,16 @@ from coloroffigure import ColorFig
 from prettytable import PrettyTable
 from math import pi
 class Circle(Geom):
-name ="круг"
-def __init__(self, radius, color):
-self.__radius = radius
-a = ColorFig()
-a.color = color
-self.__color = a.color
-def get_area(self):
-return 2 * pi * (self.__radius ** 2)
-def repr(self):
-table = PrettyTable()
-table.field_names = ["фигура","цвет","площадь"]
-table.add_row([self.name, self.__color, self.get_area()])
-print(table)
+    name ="круг"
+    def __init__(self, radius, color):
+        self.__radius = radius
+        a = ColorFig()
+        a.color = color
+        self.__color = a.color
+    def get_area(self):
+        return 2 * pi * (self.__radius ** 2)
+    def repr(self):
+        table = PrettyTable()
+        table.field_names = ["фигура","цвет","площадь"]
+        table.add_row([self.name, self.__color, self.get_area()])
+        print(table)
