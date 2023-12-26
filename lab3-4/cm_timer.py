5f7d59a3190f7a6d9e4a13f419e44ddc1f19ab26
exp008 <154838747+exp008@users.noreply.github.com>
2023-12-27 00:27:58 +0300

    Create cm_timer.py

---
+++ lab3-4/cm_timer.py
@@ -0,0 +1,20 @@
+import time
+from contextlib import ContextDecorator
+
+class cm_timer_1:
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        elapsed_time = time.time() - self.start_time
+        print(f"Elapsed time: {elapsed_time} seconds")
+
+class cm_timer_2(ContextDecorator):
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        elapsed_time = time.time() - self.start_time
+        print(f"Elapsed time: {elapsed_time} seconds")
